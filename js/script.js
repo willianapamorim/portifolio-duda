cf5025adf61d6135582a13ab47008e79f40ba49d
Willian Amorim <110067851+WillianApAmorim@users.noreply.github.com>
2025-07-01 15:48:58 -0300

    correção do menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 import { iniciarMenuHamburguer } from './menuHamburguer.js';
 import { rolagem } from './rolagem.js';
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    iniciarMenuHamburguer();
-    rolagem();
-});
+iniciarMenuHamburguer();
+rolagem();
